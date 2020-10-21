285bcd05765b72da419888bcef89391012635ea9
Martin Garrido Rodriguez-Cordoba <mgrcprof@gmail.com>
2020-10-21 20:58:39 +0200

    remake run mignon example script

--- prepareTestData.sh
+++ prepareTestData.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
-
 if [ ! -d "$PWD/mignon_test_data" ]; then
     fileid="1iH2UUF-awGPKYgixP9_5mApMpqGFDiBU";
-    filename="$PWD/mignon_test_data.tar.gz";
+    filename="$HOME/mignon_test_data.tar.gz";
     curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
     curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-    mkdir -p ${filename/.tar.gz/}
-    tar -xzvf ${filename} --directory ${filename/.tar.gz/}
+    tar -xzvf ${filename}
+    chmod 777 -R $PWD/mignon_test_data/
+    echo "MIGNON test data ---- OK"
+else 
+    echo "A directory with MIGNON test data already exists!"
 fi
-    
-sed -i "s#flagForPwd#${PWD}#g" tests/test_input.json
+
+# prepare test json
+sed "s#flagForPwd#${PWD}#g" tests/test_input.json > current_test_input.json
+chmod 777 current_test_input.json
--- runMignonExample.sh
+++ runMignonExample.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ ! -d "$HOME/misc_cache/mignon_test_data" ]; then
-    fileid="1iH2UUF-awGPKYgixP9_5mApMpqGFDiBU";
-    filename="$HOME/misc_cache/mignon_test_data.tar.gz";
-    curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-    curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-    tar -xzvf ${filename}
-fi
 
-# prepare test json
-sed -i "s#flagForPwd#${PWD}#g" tests/test_input.json
+# prepare test data
+echo "MIGNON: Preparing test data..."
+curlImage="tutum/curl:trusty"
+docker run -v ${PWD}:${PWD} -w ${PWD} ${curlImage} bash prepareTestData.sh
 
-#java -Dconfig.file=configs/LocalWithDocker.conf -jar $HOME/misc_cache/mignon_test_data/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i tests/test_input.json
+# validate input
+echo "MIGNON: Validating input JSON..."
+java -jar ${PWD}/mignon_test_data/womtool-47.jar validate wdl/MIGNON.wdl -i current_test_input.json
+
+# perform dry run
+echo "MIGNON: Performing dry run of the workflow..."
+java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i current_test_input.json
