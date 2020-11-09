ebf4d7c9ec03ff3dc70b0f55fb61c70c314069ec
Martin Garrido Rodriguez-Cordoba <mgrcprof@gmail.com>
2020-11-09 22:35:16 +0100

    Test travis mysql setting

--- runMignonExample.sh
+++ runMignonExample.sh
@@ -14,6 +14,6 @@ java -jar ${PWD}/mignon_test_data/womtool-47.jar validate wdl/MIGNON.wdl -i test
 
 # perform dry run
 echo "MIGNON: Performing dry run of the workflow..."
-java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_hisat2_vc.json
-java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_star.json
-java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_salmon.json
+java -Dconfig.file=${PWD}/tests/travisWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_hisat2_vc.json
+java -Dconfig.file=${PWD}/tests/travisWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_star.json
+java -Dconfig.file=${PWD}/tests/travisWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_salmon.json
