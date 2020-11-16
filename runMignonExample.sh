b165926a10c87b7ada4a838ff1b34a6d6362179b
Martin Garrido Rodriguez-Cordoba <mgrcprof@gmail.com>
2020-11-16 17:07:02 +0100

    Update runMignonExample.sh

--- runMignonExample.sh
+++ runMignonExample.sh
@@ -14,11 +14,13 @@ java -jar ${PWD}/mignon_test_data/womtool-47.jar validate wdl/MIGNON.wdl -i test
 
 # perform dry run
 echo "MIGNON: Performing dry run of the workflow..."
-java -Dconfig.file=${PWD}/tests/travisWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_hisat2_vc.json
-java -Dconfig.file=${PWD}/tests/travisWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_star.json
-java -Dconfig.file=${PWD}/tests/travisWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_salmon.json
-java -Dconfig.file=${PWD}/tests/travisWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON_htseq.wdl -i test_star.json
+java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_hisat2_vc.json
+java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_star.json
+java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON.wdl -i test_salmon.json
+java -Dconfig.file=${PWD}/configs/LocalWithDocker.conf -jar ${PWD}/mignon_test_data/cromwell-47.jar run wdl/MIGNON_htseq.wdl -i test_star.json
 
 # create and move all the outputs to the new directory
 mkdir -p dry_run
 mv -f test_hisat2_vc.json test_star.json test_salmon.json cromwell-execution cromwell-workflow-logs dry_run
+
+echo "MIGNON: Success!! Dry run completed. You are ready to execute MIGNON."
